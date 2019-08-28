53cd770ca52af09489b6e2afb37caae69b3f8031
Bruno Li <lisiuloong@gmail.com>
2019-08-27 20:51:58 -0400

    finish requirements to complete challenge

--- index.js
+++ index.js
@@ -8,11 +8,22 @@ function Question(question, choices, answer) {
   this.answer = answer;
 }
 
-var questionArray = [];
 init();
 
+function init(){
+    var currentQuestion = randomQuestion();
+    var userAnswer = prompt("Please select a, b or c to answer the question!");
+
+    if(userAnswer === currentQuestion.answer){
+        console.log("Correct Answer!");
+    } else {
+        console.log('Meh, sorry you got the wrong answer!');
+    }
+}
+
 // create questions objects
-function init() {
+function questions() {
+    var questionArray = [];
   // Question 1
   var question1 = new Question(
     "What type of animal is a seahorse?",
@@ -24,18 +35,17 @@ function init() {
   // Question 2
   var question2 = new Question(
     "Which of the following dogs is the smallest??",
-    { a: "Chihuahua", b: "Poodle", c: "Pomeranian" },
-    "a"
+    { a: "Poodle", b: "Chihuahua", c: "Pomeranian" },
+    "b"
   );
   questionArray.push(question2);
 
   // Question 3
-
   var question3 = new Question(
     "What color are zebras?",
     {
       a: "White with black stripes",
-      b: "lack with white stripes",
+      b: "Black with white stripes",
       c: "None of the above"
     },
     "a"
@@ -46,13 +56,17 @@ function init() {
   var question4 = new Question(
     "What existing bird has the largest wingspan?",
     { a: "Condor", b: "Swan", c: "Albatross" },
-    "a"
+    "c"
   );
   questionArray.push(question4);
+
+  return questionArray;
 }
+
 
 // generates random question when quiz starts
 function randomQuestion(){
+    var questionArray = questions();
   // pick a random question
     var currentQuestion = questionArray[Math.floor((Math.random() * questionArray.length))];
   // log question to console
@@ -61,8 +75,8 @@ function randomQuestion(){
     for (var choice in currentQuestion.choices){
       console.log(`${choice}: ${currentQuestion.choices[choice]}`);
     }
+
+    return currentQuestion;
 }
 
-// console.log(questionArray);
 
-randomQuestion();
